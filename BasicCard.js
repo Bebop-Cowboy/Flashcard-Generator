495cfb45fc6321f0653bf3128bd01a5a1ac527b0
rayjshin <rayjshin@gmail.com>
2017-07-23 22:25:47 -0400

    initial commit

---
+++ BasicCard.js
@@ -0,0 +1,27 @@
+var BasicCard = function (front, back) {
+  this.front = front;
+  this.back = back;
+}
+var card1 = new BasicCard ("tags that begin and end Javascript code within an HTML document?", "<script></script>")
+//console.log (card1);
+
+BasicCard.prototype.showFront = function (){
+  console.log (this.front);
+}
+//card1.showFront ()
+
+BasicCard.prototype.showBack = function (){
+  console.log (this.back);
+}
+//card1.showBack ()
+
+var card2 = new BasicCard ("allows the program to display a special dialog box that will notify the user that an expected event has occurred.",
+"alert()")
+
+// var BasicCards = [card1, card2];
+//  for (var i = 0; i < BasicCards.length; i++){
+//    BasicCards [i].showFront()
+//    BasicCards [i].showBack ()
+//  }
+
+module.exports = {BasicCard: BasicCard}
---
+++ flashcards.js
@@ -0,0 +1,3 @@
+var BasicCard = require ('./BasicCard').BasicCard;
+var card1 = new BasicCard ("tags that begin and end Javascript code within an HTML document?", "<script></script>");
+console.log (BasicCard);

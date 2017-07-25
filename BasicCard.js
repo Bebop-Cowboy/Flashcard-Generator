416c57a41b22141353437274088e3f7a317373b5
rayjshin <rayjshin@gmail.com>
2017-07-24 23:28:06 -0400

    updated

--- BasicCard.js
+++ BasicCard.js
@@ -18,10 +18,10 @@ BasicCard.prototype.showBack = function (){
 var card2 = new BasicCard ("allows the program to display a special dialog box that will notify the user that an expected event has occurred.",
 "alert()")
 
-// var BasicCards = [card1, card2];
-//  for (var i = 0; i < BasicCards.length; i++){
-//    BasicCards [i].showFront()
-//    BasicCards [i].showBack ()
-//  }
+var BasicCards = [card1, card2];
+ for (var i = 0; i < BasicCards.length; i++){
+   BasicCards [i].showFront()
+   BasicCards [i].showBack ()
+ }
 
 module.exports = {BasicCard: BasicCard}
